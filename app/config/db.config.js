d883236563305d6a5a67a1fe84de2298646d859e
trasherdk <trasherdk@gmail.com>
2024-06-01 15:05:55 +0200

    Add dotenv for config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
+
+const { DB_USER, DB_PASS, DB_NAME } = process.env;
+
 module.exports = {
   HOST: "localhost",
-  USER: "root",
-  PASSWORD: "123456",
-  DB: "testdb",
+  USER: DB_USER,
+  PASSWORD: DB_PASS,
+  DB: DB_NAME,
   dialect: "mysql",
   pool: {
     max: 5,
@@ -11,3 +15,4 @@ module.exports = {
     idle: 10000
   }
 };
+
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const cookieSession = require("cookie-session");
@@ -32,7 +33,10 @@ app.use(
 const db = require("./app/models");
 const Role = db.role;
 
-db.sequelize.sync();
+const { DB_FORCE, PORT } = process.env;
+db.sequelize.sync({
+  force: (DB_FORCE === 'true')
+});
 // force: true will drop the table if it already exists
 // db.sequelize.sync({force: true}).then(() => {
 //   console.log('Drop and Resync Database with { force: true }');
@@ -48,13 +52,11 @@ app.get("/", (req, res) => {
 require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
 
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
-function initial() {
+function initial () {
   Role.create({
     id: 1,
     name: "user",
